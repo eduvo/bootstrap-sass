c49cdaf950dbc8242dbd945faa1ad8b54f6bfc63
josephj <josephj6802@gmail.com>
2013-12-17 18:03:16 +0800

    Add basic stylesheets

--- bootstrap-sass.gemspec
+++ bootstrap-sass.gemspec
@@ -12,10 +12,10 @@ Gem::Specification.new do |s|
   s.license = "Apache 2.0"
 
   s.add_development_dependency 'compass'
+  s.add_development_dependency 'compass-rails', '~> 1.1.2'
   s.add_development_dependency 'term-ansicolor'
   s.add_development_dependency 'sass-rails', '>= 3.2'
   s.add_runtime_dependency     'sass',       '~> 3.2'
-
 
   s.add_development_dependency 'capybara'
   s.add_development_dependency 'poltergeist'
--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -4,6 +4,12 @@ module Bootstrap
       initializer "bootstrap-sass.assets.precompile" do |app|
         app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
       end
+      initializer 'bootstrap-sass.compass_config' do |app|
+        config = Compass::Configuration::Data.new("bootstrap-sass")
+        config.images_path = Engine.root.join('vendor', 'assets', 'images').to_s
+        config.sprite_load_path << Engine.root.join('vendor', 'assets', 'images').to_s
+        Compass.add_configuration(config)
+      end
     end
   end
 end
