8043a088011fa715333993ecc63b0bc3c66d44b8
Aaron Kuo <atk.cloud1985xp@gmail.com>
2013-12-26 22:59:34 +0800

    test, remove compass config

--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -5,11 +5,11 @@ module Bootstrap
         app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
       end
       initializer 'bootstrap-sass.compass_config' do |app|
-        config = Compass::Configuration::Data.new("bootstrap-sass")
-        config.images_path = Engine.root.join('vendor', 'assets', 'images').to_s
-        config.sprite_load_path << Engine.root.join('vendor', 'assets', 'images').to_s
-        config.relative_assets = true
-        Compass.add_configuration(config)
+        # config = Compass::Configuration::Data.new("bootstrap-sass")
+        # config.images_path = Engine.root.join('vendor', 'assets', 'images').to_s
+        # config.sprite_load_path << Engine.root.join('vendor', 'assets', 'images').to_s
+        # config.relative_assets = true
+        # Compass.add_configuration(config)
       end
     end
   end
